72b0ce2a8fbe9d14a64cca84d99370405136c081
NikolayD <kolik885@ya.ru>
2022-08-12 18:55:35 +0700

    программа нахождения максимального числа из трех

---
+++ 004/Program.cs
@@ -0,0 +1,35 @@
+﻿//Программа вывода максимального из трех чисел
+
+Console.WriteLine("Программа вывода максимального из трех чисел");
+
+Console.WriteLine("Введите первое число");
+
+string? numberString = Console.ReadLine();
+
+int numberA = int.Parse(numberString!);
+
+Console.WriteLine("Введите второе число");
+
+string? numberStringA = Console.ReadLine();
+
+int numberB = int.Parse(numberStringA!);
+
+Console.WriteLine("Введите третее число");
+
+string? numberStringB = Console.ReadLine();
+
+int numberC = int.Parse(numberStringB!);
+
+int max = numberA;
+
+if(numberB > numberA)
+{
+    max = numberB;
+}
+if(numberC > max)
+{
+    max = numberC;
+}
+
+Console.WriteLine("max = " + max);
+
