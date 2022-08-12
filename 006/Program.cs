4516809f5679fae834730706846cf7dcfe456c62
NikolayD <kolik885@ya.ru>
2022-08-12 19:34:25 +0700

    Программа к заданию №6

---
+++ 006/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа которая определяет является ли число четным
+
+Console.WriteLine("Программа которая определяет является ли число четным");
+
+Console.WriteLine("Введите число");
+
+string? numberString = Console.ReadLine();
+
+int numberA = int.Parse(numberString!);
+
+int numberB = numberA %2;
+
+if(numberB == 0)
+{
+    Console.WriteLine( + numberA + " четное число");
+}
+
+else
+{
+    Console.WriteLine( + numberA + " не четное число");
+}
+
