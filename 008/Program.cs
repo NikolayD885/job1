114bc838e7c588d2221a0670ae1759535ec96be2
NikolayD <kolik885@ya.ru>
2022-08-12 21:53:48 +0700

    Программа к заданию №8

---
+++ 008/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа которая показывает все четные числа от 1 до N
+Console.WriteLine("Программа вывода четных чисел от 1 до N");
+
+Console.WriteLine("Введите число");
+
+string? numberString = Console.ReadLine();
+
+int number = int.Parse(numberString!);
+
+for (int i = 1; i <= number; i++)
+
+if (i % 2 == 0)
+
+{
+  Console.Write(i + " "); 
+}
