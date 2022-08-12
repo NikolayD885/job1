3d57a9f904a19b39939d96bf7c808fbd58842bdf
NikolayD <kolik885@ya.ru>
2022-08-12 18:00:52 +0700

    Создание программы по заданию №2

---
+++ 002/Program.cs
@@ -0,0 +1,26 @@
+﻿//Программа нахождения большего числа из двух
+Console.WriteLine("Программа нахождения большего числа из двух");
+
+Console.WriteLine("Введите первое число");
+
+string? numberString = Console.ReadLine();
+
+int numberA = int.Parse(numberString!);
+
+Console.WriteLine("Введите второе число");
+
+string? numberStringA = Console.ReadLine();
+
+int numberB = int.Parse(numberStringA!);
+
+if(numberA < numberB)
+{
+    Console.WriteLine("max=" + numberB);
+    Console.WriteLine("min=" + numberA);
+}
+
+else
+{
+    Console.WriteLine("max=" + numberA);
+    Console.WriteLine("min=" + numberB);
+}
